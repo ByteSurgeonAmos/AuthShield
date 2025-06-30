e54f9f121b08cccd131e2720f13a7178725eee8a
XMOBIT237 <admin@xmobit.com>
2025-06-30 19:39:06 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       
       env_staging: {
         NODE_ENV: "staging",
-        PORT: 3001,
+        PORT: 3003,
         DATABASE_URL: "postgresql://datahub-admin:D4t4HubAdmin_7r9KzP2w@165.73.244.226:5432/datahub",
         TEMP_JWT_SECRET: "47411d04278145e15dfc98fc8b177786c27bf3562ebd66d3123a1eda0ebe038e",
         JWT_SECRET: "47411d04278145e15dfc98fc8b177786c27bf3562ebd66d3123a1eda0ebe038e",
@@ -30,7 +30,7 @@ module.exports = {
         JWT_SECRET: "47411d04278145e15dfc98fc8b177786c27bf3562ebd66d3123a1eda0ebe038e",
         NOTIFICATIONS_EMAIL: "no.reply@xmobit.com",
         EMAIL_PASS: "tyik2.Cp",
-        BASE_URL: "https://staging.xmobit.com",
+        BASE_URL: "https://xmobit.com",
         TIARA_API_TOKEN: "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1NzQiLCJvaWQiOjU3NCwidWlkIjoiZmJhNzdmNjEtM2ZkYy00YjFlLWI2MjAtM2I0OGI3MDg1ODhkIiwiYXBpZCI6NTQ4LCJpYXQiOjE3NDc5NDM0NzAsImV4cCI6MjA4Nzk0MzQ3MH0.FrXUlgMu_qjaNVMdtlVn6NYvinLoqkcFuHRGHvq19FF7vv7jeX9y8IQF8wGaOwKSRS5tzsxQv-4i-EbHcwbSQw",
         SMS_SENDER_ID: "CONNECT",
         FORCE_SYNC: "true"
