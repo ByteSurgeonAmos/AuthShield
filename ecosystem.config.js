651aa7b92db70fad7a637e12e86fff663eb2c7e2
Kevin Kimanzi Kilonzi <91922929+Kevinkimanzi@users.noreply.github.com>
2025-07-07 21:05:14 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,20 +27,22 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
         PORT: 3003,
-        API_KEY:"6d956be59be9acc5461b4e057e99195a0321386352d25b946b647c2f3667149e",
-        AUTH_ENGINE_API_KEY:"6d956be59be9acc5461b4e057e99195a0321386352d25b946b647c2f3667149e",
-        ADMIN_DEFAULT_PASSWORD: "xmobit@12345",
+        API_KEY:"68b7d145a58b4ceff701528ace1b95d5ef202494fc3852d5a85ead4a43f9e994",
+        ADMIN_DEFAULT_PASSWORD: "xmobit@1345",
         DATABASE_URL: "postgresql://datahub-prod-admin:Dh7\$mK9pQ2vX8nL3wR6tY4uE@86.48.1.16:5432/datahub-prod",
-        TEMP_JWT_SECRET: "47411d04278145e15dfc98fc8b177786c27bf3562ebd66d3123a1eda0ebe038e",
-        JWT_SECRET: "47411d04278145e15dfc98fc8b177786c27bf3562ebd66d3123a1eda0ebe038e",
+        TEMP_JWT_SECRET: "47411d04278145e15dfc98fc8b1777868b7d145a58b4ceff701528ace1b95d5ef202494fc3852d5a85ead4a43f9e9946c27bf3562ebd66d3123a1eda0ebe038e",
+        JWT_SECRET: "47411d0427814568b7d145a58b4ceff701528ace1b95d5ef202494fc3852d5a85ead4a43f9e994e15dfc98fc8b177786c27bf3562ebd66d3123a1eda0ebe038e",
         NOTIFICATIONS_EMAIL: "no.reply@xmobit.com",
         EMAIL_PASS: "tyik2.Cp",
         BASE_URL: "https://xmobit.com",
-
+        TIARA_BASE_URL: "https://api2.tiaraconnect.io/api/messaging/sendsms",
+        HMAC_SECRET: "pM4W33Jse6OFMbKFLjyUQweCFvGllhnD", 
         TIARA_API_TOKEN: "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI1NzQiLCJvaWQiOjU3NCwidWlkIjoiZmJhNzdmNjEtM2ZkYy00YjFlLWI2MjAtM2I0OGI3MDg1ODhkIiwiYXBpZCI6NTQ4LCJpYXQiOjE3NDc5NDM0NzAsImV4cCI6MjA4Nzk0MzQ3MH0.FrXUlgMu_qjaNVMdtlVn6NYvinLoqkcFuHRGHvq19FF7vv7jeX9y8IQF8wGaOwKSRS5tzsxQv-4i-EbHcwbSQw",
-
+        MONERO_WALLET_API_URL: "http://127.0.0.1:28084/api/v1/monero/create-fund-wallet-async",
+        BTC_API_TOKEN:"ba031e6926b09010047ac6d11936d419897b5bbbc7a2f840f279e8a2886b151e",
+        XMR_API_TOKEN: "u6HmroO3kqb4kDZdff0kl1pXlaYDqRyg",
         SMS_SENDER_ID: "CONNECT",
-        FORCE_SYNC: "true"
+        FORCE_SYNC: "false"
       }
     }
   ]
